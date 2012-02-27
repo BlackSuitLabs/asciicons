018387bf12e4cc615205244a17a3d515051e9bd1
Juan de la Torre <juan.delatorre@blacksuitlabs.com>
2012-02-26 22:29:02 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Asciicons::Application.config.secret_token = '0d4472568efe2df5e23273df831c85407873e69a6a5759f355edbf4adfa9fb485e6380d9dd60f615978a1348e0fdc4d1c0a68f9f68a735ee5e7c93fa82d245ab'
